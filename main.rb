f007441442dac9bd072d6be76dad10b0bfbd5cde
melinaDaendliker <melina.daendliker@hotmail.com>
2023-09-28 16:34:36 +0200

    add function to check if user input color exists

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 COLORS = ["blue", "red", "yellow", "green", "organe", "purple"]
 player_guess_history = {}
 
-
 def create_code_computer 
   code = []
   4.times do
@@ -16,14 +15,25 @@ def add_guess_player(player_guess_history)
   puts "Possible colors are blue, red, yellow, green, orange, purple"
   puts "Enter your guess! "
   player_guess = []
-  4.times do 
-    player_guess.push(gets.chomp)
+  4.times do
+    user_input = gets.chomp
+    user_input = correct_user_input(user_input)
+    player_guess.push(user_input)
   end
   puts `clear`
   puts "Your guess"
   return player_guess_history[player_guess] = []
 end
 
+def correct_user_input(user_input)
+  until COLORS.include? user_input
+    puts "This color is not an option"
+    puts "Enter your guess! "
+    user_input = gets.chomp
+    return user_input
+  end
+  user_input
+end
 
 code = create_code_computer
 p code
