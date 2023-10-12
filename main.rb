515acd4e3ffe34869a5ab9f41c72bbf219cf49ca
melinaDaendliker <melina.daendliker@hotmail.com>
2023-10-12 14:08:03 +0200

    correct computer_coder.rb file

--- main.rb
+++ main.rb
@@ -1,44 +1,16 @@
+require_relative 'player'
+require_relative 'computer_coder'
+require_relative 'game_play'
+require_relative 'game_modes'
+require_relative 'code_breaking'
+require 'timeout'
 
-
-require_relative "player.rb"
-require_relative "computer_coder.rb"
-require_relative "game_play.rb"
-require_relative "game_modes.rb"
-require_relative "code_breaking"
-require 'timeout' 
-
-COLORS = ["blue", "red", "yellow", "green", "orange", "purple"]
+COLORS = %w[blue red yellow green orange purple]
 play_again = true
 
-new_game = GameModes.new()
-
+new_game = GameModes.new
 
 while play_again
   new_game.chose_mode
   play_again = new_game.restart_game
 end
-
-
-
-# clean up the code 
-# use rubocop to clean up some more 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
